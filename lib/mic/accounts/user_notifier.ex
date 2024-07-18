6dfee98780207059a07b70bba8d9770ef6918bf2
Franco Abaroa <franco9395@gmail.com>
2024-07-18 11:08:54 -0400

    nit: update mail templates

--- lib/mic/accounts/user_notifier.ex
+++ lib/mic/accounts/user_notifier.ex
@@ -37,8 +37,6 @@ defmodule Mic.Accounts.UserNotifier do
   def deliver_confirmation_instructions(user, url) do
     deliver(user.email, "Confirmation instructions", """
 
-    ==============================
-
     Hi #{user.email},
 
     You can confirm your account by visiting the URL below:
@@ -46,8 +44,6 @@ defmodule Mic.Accounts.UserNotifier do
     #{url}
 
     If you didn't create an account with us, please ignore this.
-
-    ==============================
 
     Best,
     The Incurator Team
@@ -60,8 +56,6 @@ defmodule Mic.Accounts.UserNotifier do
   def deliver_reset_password_instructions(user, url) do
     deliver(user.email, "Reset password instructions", """
 
-    ==============================
-
     Hi #{user.email},
 
     You can reset your password by visiting the URL below:
@@ -69,8 +63,6 @@ defmodule Mic.Accounts.UserNotifier do
     #{url}
 
     If you didn't request this change, please ignore this.
-
-    ==============================
 
     Best,
     The Incurator Team
@@ -83,8 +75,6 @@ defmodule Mic.Accounts.UserNotifier do
   def deliver_update_email_instructions(user, url) do
     deliver(user.email, "Update email instructions", """
 
-    ==============================
-
     Hi #{user.email},
 
     You can change your email by visiting the URL below:
@@ -92,8 +82,6 @@ defmodule Mic.Accounts.UserNotifier do
     #{url}
 
     If you didn't request this change, please ignore this.
-
-    ==============================
 
     Best,
     The Incurator Team
