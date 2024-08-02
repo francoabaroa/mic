8cd35d4ad4c19935d728f94cc68713d573544da7
Franco Abaroa <franco9395@gmail.com>
2024-08-01 22:05:03 -0400

    nit: remove logs

--- lib/mic_web/plugs/check_profile.ex
+++ lib/mic_web/plugs/check_profile.ex
@@ -8,15 +8,9 @@ defmodule MicWeb.Plugs.CheckProfile do
   def init(default), do: default
 
   def call(conn, _opts) do
-    Logger.info("Plug getting called before")
-
     user_id = conn.assigns[:current_user] && conn.assigns[:current_user].id
-    Logger.info("Plug getting called")
-    Logger.info("User ID: #{user_id}")
 
     if user_id && Artists.get_profile_by_user_id!(user_id) do
-      Logger.info("Plug getting called after")
-
       conn
       |> redirect(to: "/")
       |> halt()
